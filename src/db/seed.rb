7b87684f5c9c8faa00812eea42c8f8ad95390185
EmrikbergmanNTIJohanneberg <119053225+EmrikbergmanNTIJohanneberg@users.noreply.github.com>
2024-03-07 16:01:34 +0100

    seed.rb nu klar (YIPPEEE)

--- src/db/seed.rb
+++ src/db/seed.rb
@@ -67,7 +67,10 @@ end
 def seed_tables
 
     customers = [
-        {first_name: '', last_name: '', adress: '', payment_method: ''}
+        {first_name: 'Testy', last_name: 'McTestface', adress: 'teststreet test', payment_method: 'tesh', phone: '0112345678', email: 'testy.mctestface@testmail.test', password: 'admin'},
+        {first_name: 'Sven', last_name: 'Svensson', adress: 'Svenssongatan 2', payment_method: 'Swish', phone: '0712345678', email: 'sven.svensson@hotmail.com', password: 'admin123'},
+        {first_name: 'Anders', last_name: 'Andersson', adress: 'Svenssongatan 3', payment_method: 'debit card', phone: '0723456789', email: 'anders.andersson@gmail.com', password: 'Admin123!'},
+        {first_name: 'Lars', last_name: 'Larsson', adress: 'Svenssongatan 1', payment_method: 'credit card', phone: '0734567890', email: 'lars.larsson@icloud.com', password: 'Crz0>n^g"yZx3R7IlWFWyO!SQc}<%MI>3rf4DU}}P[cp<xI'}
     ]
 
     products = [
@@ -77,10 +80,24 @@ def seed_tables
         {name: 'Prusa mk4', price: '1200', description: 'a round, orange-colored fruit that is valued mainly for its sweet juice'}
     ]
 
+    tags = [
+        {name: 'Direct drive'},
+        {name: 'Bowden drive'},
+        {name: 'All metal hotend'},
+        {name: 'PTFE-lined hotend'}
+    ]
+    
+    customers.each do |customer|
+        db.execute('INSERT INTO customers (first_name, last_name, phone, email, adress, payment_method, password) VALUES (?,?,?,?,?,?,?)', customer[:first_name], customer[:last_name], customer[:phone], customer[:email], customer[:adress], customer[:payment_method], customer[:password], )
+    end
 
     products.each do |product|
         db.execute('INSERT INTO products (name, price, description) VALUES (?,?,?)', product[:name], product[:price], product[:description])
     end
+
+    tags.each do |tag|
+        db.execute('INSERT INTO tags (name) VALUES (?)', tag[:name])
+    end    
 
 end
 
